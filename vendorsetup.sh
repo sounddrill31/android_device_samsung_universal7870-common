cbc10208fb1c79d4605f7d8c813d47a06678869e
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-06-18 10:25:56 +0530

    Try to use GCC from LineageOS 18

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
 echo "Copying Toolchain"
 # rm -rf prebuilts/zyc_clang || true
-cp -R prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 prebuilts/toolchain
+rm -rf prebuilts/toolchain || true
+# cp -R prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 prebuilts/toolchain
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 -b lineage-18.1 prebuilts/toolchain
 # git clone https://gitlab.com/clangsantoni/zyc_clang -b 14 prebuilts/zyc_clang
